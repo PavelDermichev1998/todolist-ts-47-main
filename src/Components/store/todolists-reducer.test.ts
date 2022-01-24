918a12777879d8697d4512db60007ed479a752ea
PavelDermichev1998 <pdermichev@gmail.com>
2022-01-24 18:01:03 +0300

    Reducer

--- src/Components/store/todolists-reducer.test.ts
+++ src/Components/store/todolists-reducer.test.ts
@@ -26,7 +26,6 @@ test('correct todolist should be removed', () => {
 test('correct todolist should be added', () => {
     let todolistId1 = v1();
     let todolistId2 = v1();
-
     let newTodolistTitle = "New Todolist";
 
     const startState: Array<TodoListType> = [
